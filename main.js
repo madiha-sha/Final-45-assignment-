0f1e357a37209005865e7774c0bb85bcfb353b50
Madiha shah <162611745+madiha-sha@users.noreply.github.com>
2024-03-23 09:38:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//stor a famous person name
+let famousPerson = "Quaid e Azam Muhammad Ali Jinnah";
+//storind a messege from famous person
+let messege = "kam kam or bas kam";
+console.log(`${famousPerson} - ${messege}`);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//stor a famous person name
+
+let famousPerson:string = "Quaid e Azam Muhammad Ali Jinnah";
+
+//storind a messege from famous person
+
+let messege:string="kam kam or bas kam";
+
+console.log(`${famousPerson} - ${messege}`)
